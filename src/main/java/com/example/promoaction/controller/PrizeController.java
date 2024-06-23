6ca752d20e859318d79c4dffaeb71e977dede6d0
AlexD <demyanalex@gmail.com>
2024-06-23 16:30:03 +0300

    Добавление приза в БД и в дирректорию сам файл img

--- src/main/java/com/example/promoaction/controller/PrizeController.java
+++ src/main/java/com/example/promoaction/controller/PrizeController.java
@@ -1,27 +1,63 @@
 package com.example.promoaction.controller;
 
+import com.example.promoaction.entity.Prize;
+import com.example.promoaction.service.PrizeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @Controller
+@RequestMapping("/promo-admin-panel")
 public class PrizeController {
+    private PrizeService prizeService;
 
-    @GetMapping("/promo-admin-panel/add-prize")
+    public PrizeController(PrizeService prizeService) {
+        this.prizeService = prizeService;
+    }
+
+    @GetMapping("/add-prize")
     public String addClassGet(){
+        return "redirect:/add_prize.html";
+    }
+
+    @GetMapping("/admin-panel")
+    public String adminPanelGet(){
+        return "admin_panel";
+    }
+    @GetMapping("/winners")
+    public String allPrizeGet(){
+        return "winners";
+    }
+
+    //POST REQUEST
+    @PostMapping("/add-prize")
+    public String addClassPost(
+            @RequestParam String name_prize,
+            @RequestParam String promo_code,
+            @RequestParam MultipartFile img_prize
+    ){
+        Prize prize = new Prize();
+        prize.setName_prize(name_prize);
+        prize.setPromo_cod(promo_code);
+
+        String path = prizeService.saveImageToDirAndReturnPath(img_prize);
+        prize.setImage_path(path);
+
+        prizeService.add(prize);
+
+
 
         return "redirect:/add_prize.html";
     }
 
-    @GetMapping("/promo-admin-panel/admin_panel")
-    public String adminPanelGet(){
-        return "redirect:/admin_panel";
-    }
-    @GetMapping("/promo-admin-panel/all_prize")
-    public String allPrizeGet(){
-        return "redirect:/all_prize.html";
-    }
+
+
+
 
 
 }
--- src/main/java/com/example/promoaction/service/PrizeService.java
+++ src/main/java/com/example/promoaction/service/PrizeService.java
@@ -1,9 +1,47 @@
 package com.example.promoaction.service;
 
+import com.example.promoaction.entity.Prize;
+import com.example.promoaction.repository.PrizeRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
 
 @Service
 public class PrizeService {
-    
+PrizeRepository prizeRepository;
+public PrizeService(PrizeRepository prizeRepository)
+{
+    this.prizeRepository = prizeRepository;
+}
+public List<Prize> getAllPrizes()
+{
+    return prizeRepository.findAll();
+}
+
+public void add(Prize prize) {
+    prizeRepository.save(prize);
+}
+
+public String saveImageToDirAndReturnPath(MultipartFile multipartFile){
+    String name = multipartFile.getOriginalFilename();
+    String pathDirImg = "C:\\Users\\AlexD\\IdeaProjects\\promoAction\\src\\main\\resources\\static\\img\\";
+
+    Path path = Paths.get(pathDirImg + name);
+    try {
+        Files.write(path,multipartFile.getBytes());
+    } catch (IOException e) {
+        throw new RuntimeException(e);
+    }
+    return "/img/"+name;
+
+
+}
+
+
 
 }
